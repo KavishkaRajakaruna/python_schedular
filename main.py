efa35bef77041dda9c083df0ea7590088c5a5cc8
KavishkaRajakaruna <rmkhrajakaruna@std.appsc.sab.ac.lk>
2022-05-18 06:55:00 +0530

    finished the rss feeder pulse for tech topic

---
+++ main.py
@@ -0,0 +1,3 @@
+from scheduler import run_scheduled_jobs
+
+run_scheduled_jobs()
--- scheduler.py
+++ scheduler.py
@@ -6,9 +6,10 @@ from rabbitmq_send_message import queue_broker
 
 send_queue = queue_broker()
 
-schedule.every(1).minutes.do(send_queue.send_message,'rss_reader', 'tech')
+schedule.every(15).minutes.do(send_queue.send_message,'rss_reader', 'tech')
 
-while True:
-    print('schedule running')
-    schedule.run_pending()
-    time.sleep(20)
+def run_scheduled_jobs():
+    while True:
+        print('schedule running')
+        schedule.run_pending()
+        time.sleep(20)
